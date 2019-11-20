8a7f6dd29f970c5177b50a23b93e137c5363e86b
Tijmen Brommet <tijmen@gmail.com>
2019-11-20 16:44:21 +0000

    Don't cache PR diffs

--- lib/diff.rb
+++ lib/diff.rb
@@ -1,8 +1,6 @@
 class Diff
   def self.pull_requests_between(to_sha, from_sha)
-    @compares ||= {}
-    @compares[to_sha + from_sha] ||= GitHub.client.compare('DFE-Digital/apply-for-postgraduate-teacher-training', from_sha, to_sha)
-    compare = @compares[to_sha + from_sha]
+    compare = GitHub.client.compare('DFE-Digital/apply-for-postgraduate-teacher-training', from_sha, to_sha)
 
     compare[:commits].select { |commit|
       commit[:commit][:message].start_with?("Merge pull request")
