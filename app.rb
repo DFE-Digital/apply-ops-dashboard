8211101b4dfb289ad60f29619a31c40eacea1b1a
Duncan Brown <duncanjbrown@gmail.com>
2020-03-02 12:43:45 +0000

    Bubble "confused" feature flags to the top of the table
    The screen isn't big enough to hold all our feature flags and half of
    these are out of sight. The correct fix is to remove some flags, but in
    the mean time this will help us keep sight of what's in a funny state

--- app.rb
+++ app.rb
@@ -9,6 +9,7 @@ class MyApp < Sinatra::Base
 
   get '/features' do
     @features = Features.new
+    @sorted_features = @features.all.sort_by { |f| ['confused', 'shipping', 'ok'].index(f.state) }
     erb :features
   end
 end
