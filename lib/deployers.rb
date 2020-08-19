c372ec71d7b2a43c741902c114b87524c638ad3a
Vignesh.N <vignesh.natraj@digital.education.gov.uk>
2020-08-19 15:05:17 +0100

    donot select same deployers on consecutive days
    populate a ENV['YESTERDAYS_DEPLOYER'] variable at end of day and exclude
    the person when assigning the deployer the next day

--- lib/deployers.rb
+++ lib/deployers.rb
@@ -2,7 +2,9 @@ class Deployers
   def self.for_today
     if ENV['DEPLOYERS']
       seed = Time.new.strftime('%-d%m%y').to_i
-      JSON.parse(ENV['DEPLOYERS']).shuffle(random: Random.new(seed))
+      all_deployers = JSON.parse(ENV['DEPLOYERS'])
+      all_deployers.delete(JSON.parse(File.read('yesterdays_deployer.json'))) if File.exist?('yesterdays_deployer.json')
+      all_deployers.shuffle(random: Random.new(seed))
     else
       []
     end
