d6ee307a2d3489e08a5a2203170ae59775e961ca
Vignesh.N <vignesh.natraj@digital.education.gov.uk>
2020-08-27 14:07:48 +0100

    use correct path for yesterdays_deployer.json
    the relative path when running under clockwork is the root of the container and not the /app folder
    making sure clockwork and the web app access the same file

--- clock.rb
+++ clock.rb
@@ -5,5 +5,5 @@ require_relative 'lib/notify'
 module Clockwork
   every(1.day, 'Notify.inconsistent_feature_flags', at: '12:00') { Notify.inconsistent_feature_flags }
   every(1.day, 'Notify.daily_deployment_message', at: '14:00') { Notify.daily_deployment_message }
-  every(1.day, 'Populate yesterdays deployer', at: '22:00') { File.write('/app/yesterdays_deployer.json', Deployers.for_today[0].to_json) }
+  every(1.day, 'Populate yesterdays deployer', at: '22:00') { File.write('/app/yesterdays_deployer.json', Deployers.for_today(yesterdays_deployer_file: '/app/yesterdays_deployer.json')[0].to_json) }
 end
