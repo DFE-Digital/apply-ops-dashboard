028e14cf6ff3961bb6c5f1ef68ffda0644bbb715
Vignesh.N <vignesh.natraj@digital.education.gov.uk>
2021-04-29 23:54:01 +0100

    Callback endpoint after GitHub authentication

--- app.rb
+++ app.rb
@@ -1,8 +1,10 @@
 require 'sinatra'
+require 'sinatra/cookies'
 require_relative 'lib/state'
 require_relative 'lib/features'
 
 class MyApp < Sinatra::Base
+  helpers Sinatra::Cookies
   get '/' do
     erb :index, locals: { state: State.new }
   end
@@ -11,5 +13,10 @@ class MyApp < Sinatra::Base
     @features = Features.new
     @sorted_features = @features.all.sort_by { |f| %w[confused shipping ok].index(f.state) }
     erb :features
+  end
+
+  get '/login' do
+    params.each { |k, v| cookies[k] = v }
+    redirect "/?login=success&deploy=true&environment=#{params[:environment]}&commit_sha=#{params[:commit_sha]}"
   end
 end
