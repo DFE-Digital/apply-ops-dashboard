affae9f1b5f2a0843a8189aaad37ca3b56abd5a9
Vignesh.N <vignesh.natraj@digital.education.gov.uk>
2021-04-29 12:40:15 +0100

    Show QA, Sandbox and Production

--- api.rb
+++ api.rb
@@ -15,7 +15,7 @@ class MyApi < Sinatra::Base
 
   post '/deploy-in-progress' do
     halt 404 if params['target_environment'].nil?
-    halt 422 if params['target_environment'] != 'staging' && params['target_environment'] != 'production'
+    halt 422 if params['target_environment'] != 'sandbox' && params['target_environment'] != 'production'
     Notify.prs_being_deployed(params['target_environment'])
     204
   end
--- lib/notify.rb
+++ lib/notify.rb
@@ -17,7 +17,7 @@ class Notify
 
   def self.prs_being_deployed(target_environment)
     state = State.new
-    from_environment = target_environment == 'staging' ? 'qa' : 'staging'
+    from_environment = target_environment == 'sandbox' ? 'qa' : 'sandbox'
     prs = Diff.pull_requests_between(state.latest_successfull_build_to(from_environment).commit_sha, state.latest_successfull_build_to(target_environment).commit_sha)
     Slack.post_prs_being_deployed(prs, target_environment) unless prs.empty?
   end
